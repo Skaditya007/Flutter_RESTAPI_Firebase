0b45bec953af0f71bbe99312c7dbf81cab0f316f
ADITYA <adityask345@gmail.com>
2023-09-16 23:28:58 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,66 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(
-    MyApp(),
-  );
+void main() {
+  runApp(ShoppingListApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class ShoppingListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping List',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ShoppingListScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+class ShoppingListScreen extends StatelessWidget {
+  const ShoppingListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Aditya'),
+        centerTitle: true,
+        title: Text('My Shopping List'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              _showSnackBar(context, 'Cart is empty');
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'I am Aditya',
-              style: TextStyle(fontSize: 30, color: Colors.pink),
-            ),
-          ],
-        ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 8.0),
+        children: [
+          _buildShoppingItem(Icons.shopping_basket, 'Apples'),
+          _buildShoppingItem(Icons.shopping_basket, 'Bananas'),
+          _buildShoppingItem(Icons.shopping_basket, 'Bread'),
+          _buildShoppingItem(Icons.shopping_basket, 'Milk'),
+          _buildShoppingItem(Icons.shopping_basket, 'Eggs'),
+
+        ],
+      ),
+    );
+  }
+
+  Widget _buildShoppingItem(IconData icon, String name) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(name),
+    );
+  }
+
+  void _showSnackBar(BuildContext context, String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
       ),
     );
   }
