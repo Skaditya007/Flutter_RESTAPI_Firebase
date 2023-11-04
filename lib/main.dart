fec8d5756815522955ac10fb1d7c587b45164dc8
ADITYA <adityask345@gmail.com>
2023-11-05 01:12:15 +0600

    -Stack -Align -Positioned

--- lib/main.dart
+++ lib/main.dart
@@ -1,129 +1,36 @@
+import 'package:atidya_flutter/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart'as http;
-import 'dart:convert';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: PhotoListScreen(),
-    );
-  }
-}
-
-class Photo {
-  final int id;
-  final String title;
-  final String thumbnailUrl;
-  final String url;
-
-  Photo({required this.id, required this.title, required this.thumbnailUrl, required this.url});
-
-  factory Photo.fromJson(Map<String, dynamic> json) {
-    return Photo(
-      id: json['id'],
-      title: json['title'],
-      thumbnailUrl: json['thumbnailUrl'],
-      url: json['url'],
-    );
-  }
-}
-
-class PhotoListScreen extends StatefulWidget {
-  const PhotoListScreen({super.key});
-
-  @override
-  _PhotoListScreenState createState() => _PhotoListScreenState();
-}
-
-class _PhotoListScreenState extends State<PhotoListScreen> {
-  List<Photo> photos = [];
-  bool isLoading = true;
-  String error = '';
-
-  @override
-  void initState() {
-    super.initState();
-    fetchPhotos();
-  }
-
-  Future<void> fetchPhotos() async {
-    try {
-      final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
-      if (response.statusCode == 200) {
-        final List<dynamic> data = json.decode(response.body);
-        photos = data.map((item) => Photo.fromJson(item)).toList();
-      } else {
-        error = 'Failed to load photos';
-      }
-    } catch (e) {
-      error = 'Failed to fetch data. Check your internet connection.';
-    } finally {
-      setState(() {
-        isLoading = false;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Photo Gallery App')),
-      body: isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : error.isNotEmpty
-          ? Center(child: Text(error))
-          : ListView.builder(
-        itemCount: photos.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(photos[index].title),
-            leading: Image.network(photos[index].thumbnailUrl),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PhotoDetailScreen(photo: photos[index]),
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-}
-
-class PhotoDetailScreen extends StatelessWidget {
-  final Photo photo;
-
-  const PhotoDetailScreen({super.key, required this.photo});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Photo Details')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.network(photo.url),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text('ID: ${photo.id}'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      home: HomeScreen(),
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.yellowAccent,
+            backgroundColor: Colors.pinkAccent,
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text('Title: ${photo.title}'),
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.pinkAccent,
+          centerTitle: true,
+          titleTextStyle: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+            color: Colors.yellowAccent,
           ),
-        ],
+        ),
       ),
+      title: 'Adityas App',
     );
   }
 }
